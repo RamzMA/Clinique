dd1fe1d24b79141b2f988e685cf87dacf3eeb7f4
Ramz <ramzahmedm@gmail.com>
2026-05-19 18:05:47 +0100

    Staff profile information Implemented

--- staff/models.py
+++ staff/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+#Department model for deperament name
+class Department(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+    
+class StaffProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    Department = models.ForeignKey(Department, on_delete=models.SET_NULL, null=True)
+    specialisation = models.CharField(max_length=100, blank=True)
+    licence_number = models.CharField(max_length=50, blank=True)
+    bio = models.TextField(blank=True)
+
+    def __str__(self):
+        return f'{self.user.role}: {self.user.get_full_name()}'
+    
