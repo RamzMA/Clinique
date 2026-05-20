26404316551ffb03fe7d2677c19ddf9c5be17222
Ramz <ramzahmedm@gmail.com>
2026-05-20 14:11:18 +0100

    Accounts views for login/logout & dashboard for roles implemented

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
-# Create your views here.
+def login_view(request):
+    if request.user.is_authenticated:
+        return redirect('dashboard')
+    
+    if request.method == 'POST':
+        user = authenticate(
+            request,
+            username = request.POST['username'],
+            password = request.POST['password']
+        )
+        if user:
+            login(request,user)
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid username or password.')
+        return render(request, 'accounts/login.html', {'form': True})
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+@login_required
+def dashboard(request):
+    role = request.user.role
+    templates = {
+        'ADMIN': 'dashboard/admin.html',
+        'DOCTOR': 'dashboard/doctor.html',
+        'NURSE': 'dashboard/nurse.html',
+        'PATIENT': 'dashboard/patient.html',
+    }
+    template = templates.get(role, 'dashboard/patient.html')
+    return render(request, template, {'user': request.user})
