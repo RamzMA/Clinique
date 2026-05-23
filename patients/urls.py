3f15afe1995f0a1919431d34a2c3a4b9ab888194
Ramz <ramzahmedm@gmail.com>
2026-05-23 15:22:32 +0100

    Urls for patient set up

--- patients/urls.py
+++ patients/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('patients/', views.patient_list, name='patient_list'),
+    path('patients/<int:pk>/', views.patient_detail, name='patient_detail'),
 ]
