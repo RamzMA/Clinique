0b2d7f6857f6fe983ff41773050676d5d0a8167a
Ramz <ramzahmedm@gmail.com>
2026-05-28 18:38:07 +0100

    Added staff to admin page

--- staff/admin.py
+++ staff/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Department, StaffProfile
 
-# Register your models here.
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+@admin.register(StaffProfile)
+class StaffProfileAdmin(admin.ModelAdmin):
+    list_display = ['get_full_name', 'get_department', 'specialisation']
+
+    def get_full_name(self, obj):
+        return obj.user.get_full_name()
+    get_full_name.short_description = 'Name'
+
+    def get_department(self, obj):
+        return obj.department.name if obj.department else '—'
+    get_department.short_description = 'Department'
